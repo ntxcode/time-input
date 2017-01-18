10e26113684c605a723cff50d7faf7215569a543
David Guilherme <davidlguilherme@gmail.com>
2017-01-18 20:15:16 -0300

    Add a example of the custom inputs on demo

--- demo/demo.js
+++ demo/demo.js
@@ -2,6 +2,21 @@ var React = require('react')
 var ReactDom = require('react-dom')
 var TimeInput = require('../src/TimeInput')
 
+var MyCustomInput = React.createClass({
+  getSelectionStart() {
+    return this.refs.input.selectionStart;
+  },
+  getSelectionEnd() {
+    return this.refs.input.selectionStart;
+  },
+  setSelectionRange(selectionStart, selectionEnd, selectionDirection) {
+    return this.refs.input.setSelectionRange(selectionStart, selectionEnd, selectionDirection);
+  },
+  render() {
+    return <input ref="input" {...this.props} />
+  }
+})
+
 ;(function render (value) {
   ReactDom.render((
     <TimeInput value={value} onChange={render} defaultValue='12:00:00:000 AM'/>
@@ -13,3 +28,9 @@ var TimeInput = require('../src/TimeInput')
     <TimeInput value={value} onChange={render}/>
   ), document.getElementById('demo-2'))
 })('13:00')
+
+;(function render (value) {
+  ReactDom.render((
+      <TimeInput Component={MyCustomInput} value={value} onChange={render} />
+  ), document.getElementById('demo-3'))
+})('12:00:00:000 AM')
